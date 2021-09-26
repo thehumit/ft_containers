6b4e51d6fc913a937c80b17dc9ca77d7f7269c1b
Tenebrous Quintana <tquintan@ox-u5.msk.21-school.ru>
2021-09-26 18:25:06 +0300

    aa

--- iterator.hpp
+++ iterator.hpp
@@ -1,6 +1,33 @@
 namespace ft
 {
-struct IteratorTrait {};
+    template <class Iterator>
+	struct iterator_traits
+    {
+        typedef typename Iterator::difference_type       difference_type;
+        typedef typename Iterator::value_type            value_type;
+        typedef typename Iterator::pointer               pointer;
+        typedef typename Iterator::reference             reference;
+        typedef typename Iterator::iterator_category     iterator_category;
+    };
+    
+
+    template <class T> struct iterator_traits<T*>
+    {
+        typedef ptrdiff_t                       difference_type;
+        typedef T                               value_type;
+        typedef T*                              pointer;
+        typedef T&                              reference;
+        typedef ft::random_access_iterator_tag  iterator_category;
+    };
+
+    template <class T> class iterator_traits<const T*>
+    {
+        typedef ptrdiff_t                       difference_type;
+        typedef T                               value_type;
+        typedef const T*                        pointer;
+        typedef const T&                        reference;
+        typedef ft::random_access_iterator_tag  iterator_category;
+    };
 
 template<typename It>
 class ReverseIterator:
--- vector.hpp
+++ vector.hpp
@@ -1,9 +1,17 @@
 #pragma once
+#include <exception>
+#include <iostream>
+#include <iterator>
 
 namespace ft
 {
-template typename <T, Alloc>
-class::Vector
+template typename <T, Alloc = std::allocator<T>>
+class Vector
+{
+private:
+    T* _arr;
+    size_t _size;
+    size_t capacity;
 
 
 
