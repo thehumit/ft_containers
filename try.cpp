11cd5a04bf7fb10fa8d5746cacaefa43b5fc4a1e
thehumit <A.Ryzhov25@yandex.ru>
2021-10-07 05:54:11 +0300

    commit was made четверг,  7 октября 2021 г. 05:54:11 (MSK)

---
+++ try.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+template <class T>
+
+std::ostream& operator<< (std::ostream &out, const std::vector<T> &vec)
+{
+    int size = vec.size();
+
+    for (int i = 0; i < size; i++)
+        out << "++" << vec[i] << std::endl;
+    return out;
+}
+
+
+int main(void)
+{
+	std::vector<int> v  (3, 100);
+	std::vector<int>::iterator it = v.begin();
+
+	v.insert(it, 4, 200);
+	std::cout << v << std::endl;
+
+}
+
+// template <class T>
--- vector.hpp
+++ vector.hpp
@@ -27,10 +27,105 @@ public:
     {
         return (this._size);
     }
+
     size_t capacity() const
     {
         return (this->_capacity);
     }
+
+    bool empty (void) const
+    {
+        return (size() == 0 ? true : false);
+    }
+
+    reference operator[](size_type n)
+    {
+        return (*(this->_arr + n));
+    }
+
+    const_reference operator[] (size_type n) const
+    {
+        return (*(this->_arr + n));
+    }
+
+    reference at(size_type i)
+    {
+        if (i >= this->m_size)
+            throw std::out_of_range("Out of range");
+        return (this->m_container[i]);
+    }
+
+    const_reference at(size_type i) const
+    {
+        if (i >= this->m_size)
+            throw std::out_of_range("Out of range");
+        return (this->m_container[i]);
+    }
+
+    iterator begin()
+    {
+        return (this->_arr);
+    }
+
+    const_iterator begin() const
+    {
+        return(this->_arr);
+    }
+
+    iterator end()
+    {
+        if (this->empty())
+            return (this->begin());
+        return (this->_arr + this->_size - 1);
+    }
+
+    const_iterator end() const
+    {
+        if (this->empty())
+            return (this->begin());
+        return (this->_arr + this->_size - 1);
+    }
+
+    reverse_iterator rbegin()
+    {
+        return (reverse_iterator(this->end()));
+    }
+
+    const_reverse_iterator rbegin() const
+    {
+        return (reverse_iterator(this->end()));
+    }
+
+    reverse_iterator rend()
+    {
+        return (reverse_iterator(this->begin()));
+    }
+
+    const_reverse_iterator rend() const
+    {
+        return (reverse_iterator(this->begin()));
+    }
+
+    reference front ()
+    {
+        return (*this->_arr);
+    }
+
+    reference front () const
+    {
+        return (*this->_arr);
+    }
+
+    reference back ()
+    {
+        return (*(this->_arr + size - 1));
+    }
+
+    const_reference back () const
+    {
+        return (*(this->_arr + size -1));
+    }
+
     void reszie(size_t n, type_name val = type_name())
     {
         size_t old_size = this->_size;
@@ -47,10 +142,11 @@ public:
             reserve(n);
             for (int i = old_size; i < n; i++)
             {
-                // push_back
+                push_back(val);
             }
         }
     }
+
     void reserve(size_t n)
     {
         size_t old_capacity = this->_capacity;
@@ -85,6 +181,57 @@ public:
         this->_size++;
     }
 
+    void pop_back()
+    {
+        _allocator.destroy(&this->back());
+        this->_size--;
+    }
+
+    void insert(iterator position, size_t n, const type_name &val)
+    {
+        iterator it = this->begin();
+        size_t old_capacity = this->_capacity;
+        size_t old_arr_i = 0;
+        size_t tmp_i = 0;
+
+        if (this->_size + n >= this->_capacity)
+        {
+            if ((this->_capacity * 2) > n)
+                this->capacity *= 2;
+            else
+                this->capacity = this->_size + n;
+            type_name* newarr = this->_allocator.allocate(this->_capacity);
+            for (int i = 0; it != position, i++)
+            {
+                this->_allocator.construct(newarr + i, this->_arr[i]);
+                this->_allocator.destroy(this->_arr + i);
+                old_arr_i++;
+                it++;
+            }
+            tmp_i = old_arr_i;
+            for (int i = old_arr_i + n; i < this->_size + n; i++)
+            {
+                this->_allocator.construct(newarr + i, this->_arr[old_arr_i]);
+                this->_allocator.destroy(this->_arr + old_arr_i);
+                old_arr_i++;
+            }
+            for (int j = 0; j < n; j++)
+            {
+                this->_allocator.construct(newarr + old_arr_i, val);
+                old_arr_i++;
+            }
+            this->_allocator.deallocate(this->_arr, old_capacity);
+            this->_arr = newarr;
+        }
+        else
+        {
+            while (it != position)
+                it++;
+            old_arr_i = it - this->begin();
+            type_name* newarr = this->_allocator.allocate()
+
+        }
+    }
 
 private:
     T* _arr;
